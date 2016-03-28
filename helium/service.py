1e2f66425d5df8cc74d9261e44c0a4965992144a
Marc Nijdam <marc.nijdam@gmail.com>
2016-03-28 11:44:34 -0400

    add sensor list -l to list sensors for a label

--- helium/service.py
+++ helium/service.py
@@ -14,15 +14,34 @@ class Service:
             if value: result[result_key] = value
         return result
 
-    def _get_path(self, path, params=None, *args):
-        return self._get_url(self.base_url + str.format(path, *args), params)
+    def _mk__post_body(type, attributes):
+        if not attributes or not type: return None
+        return {
+            "data": {
+                "attributes": attributes
+            },
+            "type": type
+        }
 
-    def _get_url(self, url, params = None):
+    def _mk_url(self, path, *args):
+        return self.base_url + str.format(path, *args)
+
+    def _do_url(self, method, url, params=None):
         if url is None: return None
+        params = params or {}
         headers = {'Authorization': self.api_key}
-        req = self.session.get(url, params=params, headers=headers)
+        req = self.session.request(method, url,
+                                   params=params,
+                                   headers=headers,
+                                   allow_redirects=True)
         req.raise_for_status()
         return req.json()
+
+    def _get_url(self, url,  params=None):
+        return self._do_url('GET', url, params)
+
+    def _post_url(self, url, params=None):
+        return self._do_url('POST', url, params)
 
     def _get_json_path(self, json, path):
         try: # try to get the value
@@ -30,18 +49,26 @@ class Service:
         except KeyError:
             return None
 
+    def create_sensor(self, name=None):
+        params = _mk_post_body("sensor", {
+            "name": name
+        }) if name else None
+        return self._post_url(self._mk_url('/sensor'),
+                              params=params)
+
     def get_sensors(self):
-        return self._get_path('sensor')
+        return self._get_url(self._mk_url("sensor"))
 
     def get_sensor(self, sensor_id):
-        return self._get_path('sensor/{}', None, sensor_id)
+        return self._get_url(self._mk_url('sensor/{}', sensor_id))
 
     def get_sensor_timeseries(self, sensor_id, **kwargs):
         params = self._params_from_kwargs({
             "page_id": "page[id]",
             "page_size": "page[size]"
         }, kwargs)
-        return self._get_path('sensor/{}/timeseries', params, sensor_id)
+        return self._get_url(self._mk_url('sensor/{}/timeseries', sensor_id),
+                             params=params)
 
     def get_prev_page(self, json):
         prev_url = self._get_json_path(json, ["links", "prev"])
@@ -52,13 +79,13 @@ class Service:
         return self._get_url(next_url)
 
     def get_labels(self):
-        return self._get_path('label')
+        return self._get_url(self._mk_url('label'))
 
     def get_label(self, label_id):
-        return self._get_path('label/{}', None, label_id)
+        return self._get_url(self._mk_url('label/{}', label_id))
 
     def get_elements(self):
-        return self._get_path('element')
+        return self._get_url(self._mk_url('element'))
 
     def get_element(self, element_id):
-        return self._get_path('element/{}', None, element_id)
+        return self._get_url(self._mk_url('element/{}', element_id))
--- helium_commander/commands/sensor.py
+++ helium_commander/commands/sensor.py
@@ -1,11 +1,19 @@
-import tablib
+import dpath.util as dpath
 
 def list(service, opts):
-    if not opts.sensors:
-        return service.get_sensors().get('data')
-    else:
-        return [service.get_sensor(id).get('data') for id in opts.sensors]
 
+    if opts.label:
+        labels = service.get_label(opts.label).get('data')
+        # TODO: Use include query param once available to avoid N+1 queries
+        label_sensors = dpath.values(labels, "relationships/sensor/data/*/id")
+        return [service.get_sensor(id).get('data') for id in label_sensors]
+    elif opts.sensors:
+        return [service.get_sensor(id).get('data') for id in opts.sensors]
+    else:
+        return service.get_sensors().get('data')
+
+def create(service, opts):
+    return
 
 
 def _register_commands(parser):
@@ -15,5 +23,8 @@ def _register_commands(parser):
                                  'id': 'id',
                                  'name': 'attributes/name'
                              })
-    list_parser.add_argument("sensors", metavar="SENSOR", nargs="*",
-                             help="the id of a sensor")
+    list_source_group=list_parser.add_mutually_exclusive_group()
+    list_source_group.add_argument("-s", "--sensors", metavar="SENSOR", nargs="*",
+                                   help="the id of a sensor")
+    list_source_group.add_argument("-l", "--label",
+                                   help='the id for a label')
