1fcce2bfd00310df2819ed96c6286a711a36e792
Marc Nijdam <marc.nijdam@gmail.com>
2016-04-11 08:23:49 -0700

    remove release target

--- scripts/make-release.py
+++ scripts/make-release.py
@@ -85,7 +85,7 @@ def set_setup_version(version):
 
 
 def build_and_upload():
-    Popen([sys.executable, 'setup.py', 'release', 'sdist', 'bdist_wheel', 'upload']).wait()
+    Popen([sys.executable, 'setup.py', 'sdist', 'bdist_wheel', 'upload']).wait()
 
 
 def fail(message, *args):
