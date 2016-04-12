50ddfde0bca63cf4153a7b1986efaa3c0695d6aa
Marc Nijdam <marc.nijdam@gmail.com>
2016-04-12 14:43:54 -0700

    Bump version number to 0.4.0

--- helium/__init__.py
+++ helium/__init__.py
@@ -1,3 +1,3 @@
-__version__='0.3.1-dev'
+__version__='0.4.0'
 
 from service import Service
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='helium-commander',
-    version='0.3.1-dev',
+    version='0.4.0',
     url='http://github.com/helium/helium-commander/',
     license='BSD',
     author='Marc Nijdam',
