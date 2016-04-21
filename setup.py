27608c659c808f7154ede278a84fcaa1ab4bd1fb
Marc Nijdam <marc.nijdam@gmail.com>
2016-04-21 15:31:46 -0700

    Bump version number to 0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='helium-commander',
-    version='0.4.10-dev',
+    version='0.5.0',
     url='http://github.com/helium/helium-commander/',
     license='BSD',
     author='Marc Nijdam',
