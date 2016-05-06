a6c5d8244974596a25331f474d4a6354f0f1caf0
Marc Nijdam <marc.nijdam@gmail.com>
2016-05-06 16:30:02 -0700

    Bump version number to 0.5.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as file:
 
 setup(
     name='helium-commander',
-    version='0.5.4-dev',
+    version='0.5.4',
     url='http://github.com/helium/helium-commander/',
     license='BSD',
     author='Marc Nijdam',
