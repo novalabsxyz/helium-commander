6f1ed69df2d154218c12f4f325a891d4ecc20420
Marc Nijdam <marc.nijdam@gmail.com>
2016-06-21 14:38:21 -0700

    Make writer pluggable for util.tabulate

--- helium/commands/util.py
+++ helium/commands/util.py
@@ -4,7 +4,6 @@ import sys
 import os
 import click
 import uuid
-import json
 import writer
 import urlparse
 from functools import update_wrapper
@@ -17,6 +16,7 @@ def is_uuid(str):
         return True
     except ValueError:
         return False
+
 
 def lookup_resource_id(list, id_rep, name_path=None):
     if hasattr(list, '__call__'):
@@ -43,10 +43,11 @@ def lookup_resource_id(list, id_rep, name_path=None):
                 except KeyError:
                     pass
     if len(matches) == 0:
-        raise KeyError('Id: ' + id_rep.encode('utf8')  + ' does not exist')
+        raise KeyError('Id: ' + id_rep.encode('utf8') + ' does not exist')
     elif len(matches) > 1:
         short_matches = [shorten_id(id) for id in matches]
-        raise KeyError('Ambiguous id: ' + id_rep.encode('utf8') + ' (' + ', '.join(short_matches) + ')')
+        match_list = ' (' + ', '.join(short_matches) + ')'
+        raise KeyError('Ambiguous id: ' + id_rep.encode('utf8') + match_list)
 
     return matches[0]
 
@@ -54,34 +55,42 @@ def lookup_resource_id(list, id_rep, name_path=None):
 def shorten_id(str):
     return str.split('-')[0]
 
+
 def shorten_json_id(json):
     # Ugh, reaching for global state isn't great but very convenient here
     try:
-        shorten = not click.get_current_context().find_root().params.get('uuid', False)
+        root_context = click.get_current_context().find_root()
+        shorten = not root_context.params.get('uuid', False)
     except:
         shorten = False
     json_id = json.get('id')
     return shorten_id(json_id) if shorten else json_id
 
 
-def tabulate(result, map):
+def tabulate(result, map, **kwargs):
     mapping = OrderedDict(map)
-    if not mapping or not result: return result
+    if not mapping or not result:
+        return result
 
-    # Ugh, reaching for global state isn't great but very convenient here
-    default_format = 'tty' if sys.stdout.isatty() else 'csv'
-    format = click.get_current_context().params.get('format')
-    # Ensure format is set to something sensible
-    format = format or default_format
-    file = click.utils.get_text_stream('stdout')
-    output = writer.for_format(format, file, mapping=mapping)
-    output.start()
-    output.write_entries(result)
-    output.finish()
+    _writer = kwargs.pop('writer', None)
+    if not _writer:
+        # Ugh, reaching for global state isn't great but very convenient here
+        default_format = 'tty' if sys.stdout.isatty() else 'csv'
+        format = click.get_current_context().params.get('format')
+        # Ensure format is set to something sensible
+        format = format or default_format
+        file = click.utils.get_text_stream('stdout')
+        _writer = writer.for_format(format, file, mapping=mapping)
+
+    _writer.start()
+    _writer.write_entries(result)
+    _writer.finish()
+
 
 def map_script_filenames(json):
     files = dpath.get(json, 'meta/scripts')
     return ', '.join(extract_script_filenames(files))
+
 
 def extract_script_filenames(files):
     return [urlparse.urlsplit(url).path.split('/')[-1] for url in files]
@@ -91,15 +100,15 @@ CONTEXT_SETTINGS = dict(
 )
 
 
-
 def cli(version=None, package=None,  commands=None):
     class Loader(click.MultiCommand):
         def list_commands(self, ctx):
             commands.sort()
             return commands
+
         def get_command(self, ctx, name):
             try:
-                command = import_module(package +"."+name)
+                command = import_module(package + "." + name)
                 return command.cli
             except ImportError, e:
                 click.secho(str(e), fg='red')
@@ -108,7 +117,8 @@ def cli(version=None, package=None,  commands=None):
     def decorator(f):
         @click.option('--uuid', is_flag=True,
                       help="Whether to display long identifiers")
-        @click.option('--format', type=click.Choice(['csv', 'json', 'tty']), default=None,
+        @click.option('--format', type=click.Choice(['csv', 'json', 'tty']),
+                      default=None,
                       help="The output format (default 'tty')")
         @click.version_option(version=version)
         @click.command(cls=Loader, context_settings=CONTEXT_SETTINGS)
