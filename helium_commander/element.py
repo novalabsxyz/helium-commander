8302a690bbbe38217544fcf83f5d6a34cc75e8b2
Marc Nijdam <marc.nijdam@gmail.com>
2016-09-21 14:37:11 -0700

    Add last-seen to element information (#12)

--- helium_commander/element.py
+++ helium_commander/element.py
@@ -10,10 +10,18 @@ def display_map(cls, client):
     def _count_sensor(self):
         return len(self.sensors(use_included=True))
 
+    def _seen(self):
+        try:
+            return self.meta.last_seen
+        except AttributeError:
+            pass
+        return None
+
     dict = super(Element, cls).display_map(client)
     dict.update([
         ('mac', _mac),
         ('sensors', _count_sensor),
+        ('seen', _seen),
         ('name', attrgetter('name')),
     ])
     return dict
