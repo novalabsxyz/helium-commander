32c7dedf56c52ef9aac48f379d45af521ce42b50
Marc Nijdam <marc.nijdam@gmail.com>
2016-09-23 17:02:31 -0700

    Add coverate for element display_map changes

--- tests/test_element.py
+++ tests/test_element.py
@@ -4,12 +4,17 @@ from helium_commander import Element, Sensor
 
 
 def test_display_map(client):
-    elements = Element.all(client, include=[Sensor])
-    assert len(elements) > 0
-
     display_map = Element.display_map(client)
     assert display_map is not None
 
-    element = elements[0]
-    values = [f(element) for f in display_map.values()]
-    assert values is not None
+    def validate_display_map(element):
+        values = [f(element) for f in display_map.values()]
+        assert values is not None
+
+    elements = Element.all(client, include=[Sensor])
+    assert len(elements) > 0
+    validate_display_map(elements[0])
+
+    elements = Element.all(client)
+    assert len(elements) > 0
+    validate_display_map(elements[0])
