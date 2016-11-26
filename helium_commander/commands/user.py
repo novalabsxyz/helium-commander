1d28d8d4c0108826447e310221b664ab44aecaca
Marc Nijdam <marc@helium.com>
2016-11-26 10:14:25 -0800

    Add user update

--- helium_commander/commands/user.py
+++ helium_commander/commands/user.py
@@ -1,6 +1,6 @@
 import click
 from helium_commander import Client, User
-
+from collections import OrderedDict
 
 pass_client = click.make_pass_decorator(Client)
 
@@ -20,6 +20,23 @@ def list(client):
     Lists information associated withe the user for the current api key.
     """
     user = User.singleton(client)
+    User.display(client, [user])
+
+
+@cli.command()
+@click.option('--name',
+              help="the new name for the user")
+@pass_client
+def update(client, name):
+    """Update the attributes of the user.
+
+    Updates the attributes of the currently authorized user.
+    """
+    user = User.singleton(client)
+    attributes = OrderedDict()
+    if name:
+        attributes['name'] = name
+    user = user.update(attributes=attributes)
     User.display(client, [user])
 
 
--- tests/commands/test_user.py
+++ tests/commands/test_user.py
@@ -4,3 +4,12 @@ from .util import cli_run
 def test_list(client, authorized_user):
     output = cli_run(client, ['user', 'list'])
     assert authorized_user.short_id in output
+
+
+def test_update(client, authorized_user):
+    name = authorized_user.name
+    output = cli_run(client, ['user', 'update',
+                              '--name', 'name update'])
+    assert 'name update' in output
+    output = cli_run(client, ['user', 'update',
+                              '--name', name])
