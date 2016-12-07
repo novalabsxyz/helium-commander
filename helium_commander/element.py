7019e83f73a3a3915b3e42e208e14a2a30291ba3
Marc Nijdam <marc@helium.com>
2016-12-07 11:22:10 -0800

    Add connected state to element list

--- helium_commander/element.py
+++ helium_commander/element.py
@@ -10,11 +10,11 @@ def display_map(cls, client, include=None):
         return len(self.sensors(use_included=True))
 
     def _seen(self):
-        try:
-            return self.meta.last_seen
-        except AttributeError:
-            pass
-        return None
+        return getattr(self.meta, 'last_seen', None)
+
+    def _connected(self):
+        state = getattr(self.meta, 'connected', None)
+        return 'yes' if state is True else 'no'
 
     def _name(self):
         return getattr(self, 'name', None)
@@ -28,6 +28,7 @@ def display_map(cls, client, include=None):
         dict['sensors'] = _count_sensor
 
     dict.update([
+        ('connected', _connected),
         ('seen', _seen),
         ('name', _name)
     ])
