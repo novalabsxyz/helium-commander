938cb07e1949c54b89e573f7c6556de88750055f
Marc Nijdam <marc.nijdam@gmail.com>
2017-01-24 15:41:13 -0800

    Feature/label element (#17)
    * Adds element support for labels
    
    This adds support for managing elements as part of
    labels. Adding/removing/replacing sensors and elements is now done in
    the `label sensor` and `label element` commands and is no longer
    supported in `label update`
    
    * Update Changelog

--- helium_commander/label.py
+++ helium_commander/label.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-from helium import Label, Sensor
+from helium import Label, Sensor, Element
 from operator import attrgetter
 
 
@@ -7,9 +7,14 @@ def display_map(cls, client, include=None):
     def _count_sensor(self):
         return len(self.sensors(use_included=True))
 
+    def _count_element(self):
+        return len(self.elements(use_included=True))
+
     dict = super(Label, cls).display_map(client)
     if include and Sensor in include:
         dict['sensors'] = _count_sensor
+    if include and Element in include:
+        dict['elements'] = _count_element
     dict.update([
         ('name', attrgetter('name'))
     ])
